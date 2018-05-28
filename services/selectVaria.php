7ef79a2d5f8603dfc0738263adb0cae09fc39b71
ffgt <unah20121015078@gmail.com>
2018-05-28 14:36:25 -0600

    creación de selectVaria

--- services/selectVaria.php
+++ services/selectVaria.php
@@ -1,2 +1,6 @@
 <?php
-    include()
+    include('includes/funciones.php');
+    session_start();
+    $sql3="select count(mensaje) as num from mensajes where visto=0 and origen=0 and cuenta=?";
+    $json=select($sql3,array($_SESSION["data"]["cuenta"]));
+    echo json_encode($json);
