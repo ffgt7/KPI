197ada091487cae8713011be83204ecc096466a1
F_T <unah20121015078@gmail.com>
2018-05-29 15:59:13 -0600

    agregado cargarData

--- controller/user/index.js
+++ controller/user/index.js
@@ -5,20 +5,24 @@ app.controller('index', function($scope, $rootScope, $http) {
 
     var listVaria;
 
-    $http({
-        method: "POST",
-        url: "services/selectData.php",
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
-        }
-    }).then(
-        function success(response) {
-            $scope.dataResult = response.data;
-        },
-        function error(response) {
-            alert('Se produjo un error');
-        }
-    );
+    cargarData();
+    function cargarData(){
+        $http({
+            method: "POST",
+            url: "services/selectData.php",
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
+            }
+        }).then(
+            function success(response) {
+                $scope.dataResult = response.data;
+            },
+            function error(response) {
+                alert('Se produjo un error');
+            }
+        );
+    }
+
 
     $http({
         method: "POST",
@@ -49,6 +53,7 @@ app.controller('index', function($scope, $rootScope, $http) {
         }).then(
             function success(response) {
                 alert('Datos guardados correctamente');
+                cargarData();
             },
             function error(response) {
                 alert('Se produjo un error al cargar las variables');
